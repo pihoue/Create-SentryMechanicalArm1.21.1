62969af822b3e60e4b63307a71d0f959a0132c03
Create-SentryMechanicalArm <zxcvbnm126zxcvbnm163@gmail.com>
2026-05-12 14:27:43 +0800

    Restore DeployerHandlerMixin for Create/TACZ deployer integration

---
+++ src/main/java/euphy/upo/sentrymechanicalarm/mixin/DeployerHandlerMixin.java
@@ -0,0 +1,11 @@
+package euphy.upo.sentrymechanicalarm.mixin;
+
+import com.simibubi.create.content.kinetics.deployer.DeployerHandler;
+import com.tacz.guns.api.item.IAmmo;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(DeployerHandler.class)
+public abstract class DeployerHandlerMixin {
+
+}
